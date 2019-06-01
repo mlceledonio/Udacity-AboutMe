e9f2bd51c9fbdbfb393fa4180cb52b4f5d985181
Messiah Celedonio <celedoniomessiah@gmail.com>
2019-06-01 21:16:24 +1000

    Added kapt plugin to gradle Added data binding functionality to main

--- app/src/main/java/com/example/mesha/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/mesha/aboutme/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.example.mesha.aboutme
 
+import android.content.Context
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.example.mesha.aboutme.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        /*findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
+        }*/
+        binding.doneButton.setOnClickListener{
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View){
+
+        binding.apply {
+            nicknameText.text = nicknameEdit.text
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            view.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
+
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
